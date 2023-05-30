c600b89ef562ab002d4e6dfb60d8442b3e355190
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-05-30 22:51:18 +0600

    loaded single user data

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -1,12 +1,15 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const Update = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const loadedData = useLoaderData();
+
+  return (
+    <div>
+      <h2>User Information: {loadedData.name}</h2>
+    </div>
+  );
 };
 
 export default Update;
--- src/main.jsx
+++ src/main.jsx
@@ -20,6 +20,7 @@ const router = createBrowserRouter([
   {
     path: "/update/:id",
     element: <Update></Update>,
+    loader: ({ params }) => fetch(`http://localhost:5000/users/${params.id}`),
   },
 ]);
 
