aab326b8af3e843c7550dfd1b545dc4a84568a34
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-06-01 01:20:25 +0600

    67.8

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -11,6 +11,19 @@ const Update = () => {
     const name = form.name.value;
     const email = form.email.value;
     console.log(name, email);
+    const updatedUser = { name, email };
+
+    fetch(`http://localhost:5000/users/${loadedData._id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(updatedUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
   };
 
   return (
